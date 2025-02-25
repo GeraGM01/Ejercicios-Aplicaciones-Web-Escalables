6860fcd4838ebd2e316f636854d1b384eb55c5f1
Gerardo Monsiváis <gerazmonsivais@gmail.com>
2025-02-24 23:06:07 -0600

    Carga de tarea 1

---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: false,
+  
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  contador: number = 107;
+
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
+}
